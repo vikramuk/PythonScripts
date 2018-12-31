26ed2fe67eed025b4287165c381233013216d44b
vikram u k <vikramuk@gmail.com>
2018-12-31 15:57:08 +0530

    Update AutoItPython.py

--- AutoItPython.py
+++ AutoItPython.py
@@ -1,131 +1,73 @@
 #AutoIt Repeat Runs
 #https://www.3pillarglobal.com/insights/automated-browser-performance-testing-using-python-and-autoit
 # Python 2.7 32b, Pip + Setup Tools, PyWin32 v218 for Python 2.7 32b, xlwt module by pip install
-from subprocess import Popen, PIPE
+from subprocess import Popen
 from test_base import TestBase
 import datetime
 import os
 import time
-import win32com.client
 import xlwt
 
 
-class TestLocalWebpageLoadTime(TestBase):
+class TestStartupShutdownTime(TestBase):
 
-
-    def __init__(self):
-        self.autoIt = win32com.client.Dispatch("AutoItX3.Control")
-        self.autoIt.AutoItSetOption("WinTitleMatchMode", 4)
-        
-
-    def open_webpage_in_browser(self, address):
-        Popen("c:\Program Files\Internet Explorer\iexplore.exe %s" %address)
-        
-    def extract_load_time_from_title(self, full_title):
-        elements = full_title.split(" ")
-        return float(elements[2])/1000
-   
-    def get_value_from_wmic_output(self, proc):
-        return float(str(proc.communicate()).split()[1].split('\\n')[-1])
-    
-    def get_cpu_load_percentage(self):
-        proc = Popen('wmic cpu get LoadPercentage',stdout=PIPE, stderr=PIPE)
-        return self.get_value_from_wmic_output(proc)
-    
-    def get_mem_load_percentage(self):
-        proc_free = Popen('wmic os get FreePhysicalMemory',stdout=PIPE, stderr=PIPE)
-        proc_total = Popen('wmic ComputerSystem get TotalPhysicalMemory',stdout=PIPE, stderr=PIPE)
-        free = self.get_value_from_wmic_output(proc_free)
-        total = self.get_value_from_wmic_output(proc_total)/1024
-        used = total-free
-        load_percent = used/total*100 
-        return round(load_percent, 1)
-   
-	def search_window_with_title_containing(self, string_in_title):
-        mainWindowHandle = self.autoIt.WinGetHandle(string_in_title)
-        testHandle = "[HANDLE:%s]" %mainWindowHandle
-        full_title = self.autoIt.WinGetTitle(testHandle)
-        return full_title
-
-    def wait_for_browser_window_to_appear(self, string_in_title, timeout):
+    def launch_native_browser(self):
+        Popen("c:\Program Files\Internet Explorer\iexplore.exe")
+      
+    def wait_for_browser_window_to_appear(self, window_title, timeout):
         initial_timeout = timeout
-        full_title = self.search_window_with_title_containing(string_in_title)
-        while (full_title == '0') and (timeout > 0):
-            timeout -= 0.5
-            time.sleep(0.5)
-            full_title = self.search_window_with_title_containing(string_in_title)
+        while (self.search_window_with_title_containing(window_title) == '0') and (timeout > 0):
+            timeout -= 0.1
+            time.sleep(0.1)
         if timeout == 0:
-            raise Exception('Window with title containing "%s" did not appear in %ds.' %(string_in_title, initial_timeout))
+            raise Exception('Browser did not launch in the maximum timeout of %ds.' %initial_timeout)
         else:
-            return full_title
-
-	def write_results_to_excel_sheet(self, book, results, sheet_name):
-        sheet = book.add_sheet(sheet_name)
-        sheet.write(0, 1, sheet_name)
-        i=1
-        for res in results:
-            sheet.write(i, 0, "Run %d" %i)
-            sheet.write(i, 1, res)
-            i += 1
-
-	def close_browser_window(self, substring_in_title, timeout):
+            startup_time = initial_timeout - timeout
+            return round(startup_time, 2)
+   
+    def close_browser_window(self, window_title, timeout):
         initial_timeout = timeout
-        full_title = self.search_window_with_title_containing(substring_in_title)
-        while full_title != '0':
-            self.autoIt.WinClose(full_title)
-            timeout -= 1
-            time.sleep(1)
-            full_title = self.search_window_with_title_containing(substring_in_title)
+        while self.search_window_with_title_containing(window_title) != '0':
+            self.autoIt.WinClose(window_title)
+            timeout -= 0.1
+            time.sleep(0.1)
         if timeout == 0:
-            raise Exception('Window with title containing "%s" could not be closed in a %ds interval.' %initial_timeout)
-       
-    def clear_cache(self):
-        folder_native = ""
-        folder_guest = ""
-        if "Windows-XP" in platform.platform():
-            folder_native = os.environ['USERPROFILE'] + "\Local Settings\Temporary Internet Files"
-            folder_guest = os.environ['USERPROFILE'] + "..."
-        elif "Windows-7" in platform.platform():
-            folder_native = os.environ['USERPROFILE'] + "\AppData\Local\Microsoft\Windows\Temporary Internet Files"
-            folder_guest = os.environ['APPDATA'] + "..."
-        os.system("cd %s & rd /s /q ." %folder_native)
-        if os.path.exists(folder_guest):
-            os.system("cd %s & rd /s /q ." %folder_guest)
-
+            raise Exception('Browser window could not be closed in the maximum timeout of %ds.' %initial_timeout)
+        else:
+            shutdown_time = initial_timeout - timeout 
+            return round(shutdown_time, 2)
+   
  
-	def run_one_sequence(self):
-        self.clear_cache()
-        self.open_webpage_in_browser("http://xxx/wordpress/")
-        full_title = self.wait_for_browser_window_to_appear("Loading time:", 60)
-        load_time = self.extract_load_time_from_title(full_title)
-        cpu_load = self.get_cpu_load_percentage()
-        mem_load = self.get_mem_load_percentage()
-        self.close_browser_window(full_title, 20)
-        return load_time, int(cpu_load), mem_load
-    
-	   
-    def run_test(self, nb_of_runs):
-        all_times = []
-        all_cpu_loads = []
-        all_mem_loads = []
+
+    def run_one_sequence(self, environment):
+        if environment == "native":
+            self.launch_native_browser()
+        elif environment == "custom":
+            self.launch_custom_browser()
+        startup_time = self.wait_for_browser_window_to_appear("Blank Page", 60)
+        shutdown_time = self.close_browser_window("Blank Page", 60)
+        return startup_time, shutdown_time
+
+    def run_test(self, nb_of_runs, environment):
+        startup_times = []
+        shutdown_times = []
         
         for i in xrange(nb_of_runs):
-            try:
-                t, cpu_load, mem_load = self.run_one_sequence()
-                all_times.append(t)
-                all_cpu_loads.append(cpu_load)
-                all_mem_loads.append(mem_load)
-                time.sleep(5) # pause between runs
-            except:
-                time.sleep(5)
-                os.system("taskkill /IM iexplore.exe /T") # if page is not fully loaded in the given timeout, kill the crashed IE process
-                time.sleep(1)
-                continue
-        self.clear_cache()
-         
+            startup, shutdown = self.run_one_sequence(environment)
+            startup_times.append(startup)
+            shutdown_times.append(shutdown)
+            time.sleep(5)
+            
         book = xlwt.Workbook(encoding="utf-8")
-        self.write_results_to_excel_sheet(book, all_times, "All Load Times(s)")
-        self.write_results_to_excel_sheet(book, all_cpu_loads, "CPU Loads (%)")
-        self.write_results_to_excel_sheet(book, all_mem_loads, "MEM Loads (%)")
+        self.write_results_to_excel_sheet(book, startup_times, "Startup time(s) - " + environment.upper())
+        self.write_results_to_excel_sheet(book, shutdown_times, "Shutdown times(s) - " + environment.upper())
         time_stamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        book.save("results/LocalWebpageLoadTime-WIN7-CUSTOM-%s-%s.xls" %(self.installed_tool_version(), time_stamp))
+        
+        book.save("results/BrowserStartupShutdownTime-WIN7-%s-%s-%s.xls" %(environment.upper(), self.installed_tool_version(), time_stamp))    
+        
+    
+        
+if __name__ == '__main__':
+    TestStartupShutdownTime().run_test(1000, "native")
+    TestStartupShutdownTime().run_test(1000, "custom")
+
