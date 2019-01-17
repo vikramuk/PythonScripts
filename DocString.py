cd980e16c7e9ff2237aab9c09e3b11f25d007fad
vikram u k <vikramuk@gmail.com>
2019-01-17 16:36:43 +0530

    Update DocString.py

--- DocString.py
+++ DocString.py
@@ -13,4 +13,32 @@ print __doc__
 help(__name__)
 #help (Test)
 print "DocString", DocString.__doc__
+#             This File is for Document in Comments 
+''' __Doc__ Strings are here '''
+class Simple():
+    ''' Simple-Class __Doc__String'''
+
+    def __init__(self, value):
+        '''Return Initial value'''
+        self.value = value
+
+    def double(self):
+        ''' Double Initial Value'''
+        return 2* self.value
+
+    def __mul__(self, value):
+        ''' Multiplier Method'''
+        return self.value * value
+
+def triple(value):
+    ''' triple Value '''
+    return value*3
+
+simple = Simple(10)
+print "Double" , simple.double()
+print "Triple", triple(simple)
+help(__name__)
+help( simple.double)
+
+
     
